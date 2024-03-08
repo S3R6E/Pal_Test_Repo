cca870ef42a30e46d627f36711e561ae3681d2cd
Genesis <gzustares@gmail.com>
2024-03-08 15:53:59 +0800

    fix

--- R/datawranglingRC.R
+++ R/datawranglingRC.R
@@ -132,6 +132,11 @@ data_rc_cover <-
   left_join(location_lookup,
             by="site_reef_name")
 
+ # filter(type == "point_machine_classification") |>
+ # mutate(cover = count_groupcode / total * 100) %>% 
+ # mutate(date = as.Date(survey_start_date, "%Y-%m-%d"))
+
+
 data_rc_cover |> as.data.frame() |> glimpse()
 
 
@@ -159,3 +164,13 @@ data_rc_cover |>
   geom_point()
 
 write_csv(data_rc_cover, file="../data/processed/coral_cover_rc.csv")
+
+data_rc_cover %>%
+  ggplot(aes (y = cover, x = survey_start_date)) +
+  geom_point()
+
+data_rc_cover %>% 
+  filter(type=="point_machine_classification") %>% 
+  ggplot(aes(y = cover, x = date, colours=site_reef_type, group=site_reef_zone)) %>% 
+  geom_line()+
+  geom_point()
