7b9b9dd26b2b70cfb8e5bf154e0c132bdc91ce23
KC Lape <karizzalape@gmail.com>
2024-03-08 14:28:03 +0800

    added some more notes

--- R/ExerciseCD.R
+++ R/ExerciseCD.R
@@ -5,8 +5,8 @@ source("functions.R")
 ## Read in the data from the primary data folder
 data1 <- read_csv("../data/primary/Siete Picados_T1.csv")
 
-
+##traditional way of using a function
 glimpse(data1)
-##modern way of function
+##modern way of using a function
 data1 |> glimpse()
 data1
