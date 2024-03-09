8c10e50066a65638559647543ab909f12e31f754
Murray Logan <i.obesulus@gmail.com>
2024-03-09 11:09:09 +1000

    Replace Menu.R with 00_main.R

---
+++ R/00_main.R
@@ -0,0 +1,19 @@
+# Palawan Test Repository
+
+## - Question 1: live coral cover condition over time and space
+##   - data needs: Coral cover at sites over time
+source("q1.R")
+## - Question 2: live coral diversity condition over time and space
+##   - data needs: Coral cover at sites over time
+source("q2.R")
+## - Question 3: effect of MPA's on coral cover/diversity
+##   - data needs: Coral cover at both MPA and non-MPA sites
+source("q3.R")
+## - Question 6: impact of tourism on coral cover
+##   - data needs: sites along a tourism gradient (or tourism/non-tourism sites)
+source("q6.R")
+## - Question 4: coral community composition over time and space
+##   - data needs: Coral cover at sites over time
+source("q4.R")
+## - Question 5: ranking of taxa in space and time
+source("q5.R")
