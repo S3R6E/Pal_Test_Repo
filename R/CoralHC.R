26ef0bde4681708adc3e4161aa17d4074020ffc2
kaycac <workdarellekaye@gmail.com>
2024-03-11 15:06:52 +0800

    updated updated

--- R/CoralHC.R
+++ R/CoralHC.R
@@ -133,18 +133,62 @@ ggsave(file = "../outputs/figures/tourist_access_plot1.png",
 ggsave(file = "../outputs/figures/tourist_access_plot1.pdf",
        width = 7, height = 5, units = "in")
 
+library(rstan)
 library(brms)
+library(DHARMa)
+library(patchwork)
 
 glimpse(all_data)
 
-form <- bf(count_groupcode | trials(total) ~ tourist_access + (1 | Site) + (1| Transect),
+set.seed(3001)
+
+form <- bf(count_groupcode | trials(total) ~ tourist_access + (1 | Site),
            family = binomial(link = "logit"))
+
+get_prior(form, data=all_data)
+
+priors <- prior(normal(0,1), class = "Intercept") +
+  prior(normal(0,1), class = "b") +
+  prior(student_t(3,0,2), class = "sd")
+
 model1 <- brm(form, 
-              data = all_data)
+              data = all_data,
+              prior = priors,
+              chains = 3,
+              iter = 4000,
+              warmup = 2000,
+              thin = 10,
+              sample_prior = "only",
+              backend = "rstan")
+
+model1 |> conditional_effects() |> plot()
+
+model1 <- model1 |> 
+  update(sample_prior = "yes")
+
 model1 |> conditional_effects() |> plot()
 
 model1 |> plot()
 
-get_prior(form, data=all_data)
-
 summary(model1)
+
+model1$fit |> stan_trace()
+
+model1$fit |> stan_ac()
+
+model1$fit |> stan_rhat()
+
+model1$fit |> stan_ess()
+
+model1 |> pp_check(type = "dens_overlay", ndraws = 100)
+
+resids <- model1 |> make_brms_dharma_res(integerResponse = FALSE)
+
+testUniformity(resids)
+
+plotResiduals(resids, form = factor(rep(1, nrow(all_data))))
+
+plotResiduals(resids)
+
+testDispersion(resids)
+``
