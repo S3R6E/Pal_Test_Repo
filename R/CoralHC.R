0933988253b9edaf065d517584d33106d5c4ef44
Dan <danclarkbacani@gmail.com>
2024-03-12 12:59:55 +0800

    draft

--- R/CoralHC.R
+++ R/CoralHC.R
@@ -1,30 +1,24 @@
 library(tidyverse)
 
-<<<<<<< HEAD
 source("../Pal_Test_Repo/R/functions.R")
-=======
 
-source("C:/Users/asust/OneDrive/Desktop/Rstudio/Pal_Test_Repo/Copyoffunctions.R")
->>>>>>> 5ab1b7f0cd791e66a48ba4abe3060336a0f22963
 
 ##reading the data from the primary data folder
 data <- read_csv("../Pal_Test_Repo/data/primary/DiveSite_AbdeensRock_T1.csv")
 glimpse(data)
 data_T2 <- read_csv("../Pal_Test_Repo/data/primary/DiveSite_AbdeensRock_T2.csv")
 glimpse(data_T2)
-<<<<<<< HEAD
 data_T3 <- read_csv("../Pal_Test_Repo/data/primary/DiveSite_AbdeensRock_T3.csv")
 data_PT1 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T1.csv")
 data_PT2 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T2.csv")
 data_PT3 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T3.csv")
 
 ##checking data
-=======
-data_T3 <- read_csv("../data/primary/DiveSite_AbdeensRock_T3.csv")
-data_PT1 <- read_csv("../data/primary/Protected_MitriRock_T1.csv")
-data_PT2 <- read_csv("../data/primary/Protected_MitriRock_T2.csv")
-data_PT3 <- read_csv("../data/primary/Protected_MitriRock_T3.csv")
->>>>>>> 5ab1b7f0cd791e66a48ba4abe3060336a0f22963
+data_T3 <- read_csv("../Pal_Test_Repo/data/primary/DiveSite_AbdeensRock_T3.csv")
+data_PT1 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T1.csv")
+data_PT2 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T2.csv")
+data_PT3 <- read_csv("../Pal_Test_Repo/data/primary/Protected_MitriRock_T3.csv")
+
 glimpse(data)
 
 
@@ -103,7 +97,6 @@ data_PT2 <- data_PT2 |>
   mutate('Tourist Access'= "no")
 
 
-
 ##Protected Transect 3
 data_PT3 <- data_PT3 |>
   cpce_classif_to_points() |>
@@ -124,13 +117,9 @@ all_data <- bind_rows(data,
                       data_T3,
                       data_PT1,
                       data_PT2,
-<<<<<<< HEAD
-                      data_PT3) |> 
-  dplyr::rename(tourist_access = 'Tourist Access')
-=======
                       data_PT3) |>
   dplyr::rename(tourist_access = `Tourist Access`)
->>>>>>> 8ea8d0f00a93e74bba6572529d8e837b80992256
+
  
 ## Exploratory data analysis
 
@@ -153,8 +142,6 @@ ggsave(file = "../outputs/figures/tourist_access_plot1.png",
 ggsave(file = "../outputs/figures/tourist_access_plot1.pdf",
        width = 7, height = 5, units = "in")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 library(brms)
 
 glimpse(all_data)
@@ -164,16 +151,18 @@ form <- bf(count_groupcode | trials(total) ~ tourist_access + (1 | Site) + (1| T
 
 model1 <- brm(form,
               data=all_data)
-=======
-=======
+
 library(tidyverse)
->>>>>>> 5ab1b7f0cd791e66a48ba4abe3060336a0f22963
 library(rstan)
 library(brms)
 library(tidybayes)
 library(DHARMa)
 library(emmeans)
 library(patchwork)
+library(knitr)
+library(HDInterval)
+
+all_data <- read_csv("../Pal_Test_Repo/data/processed/q6_all_data.csv")
 
 glimpse(all_data)
 
@@ -184,13 +173,13 @@ get_prior(form, data=all_data)
 
 priors <- prior(normal(0,1), class = "Intercept") +
   prior(normal(0,1), class = "b") +
-  prior(student_t(3,0,3), class = "sd")
+  prior(student_t(3,0,1), class = "sd")
 
 model1 <- brm(form, 
               data = all_data,
               prior = priors,
               chains = 3,
-              iter = 4000,
+              iter = 2000,
               warmup = 1000,
               thin = 10,
               sample_prior = "only",
@@ -201,36 +190,52 @@ model1 |> conditional_effects() |> plot()
 model1 <- model1 |> 
   update(sample_prior = "yes")
 
->>>>>>> 8ea8d0f00a93e74bba6572529d8e837b80992256
 model1 |> conditional_effects() |> plot()
 
 model1 |> plot()
 
-<<<<<<< HEAD
-form <- bf(count_groupcode | trials(total) ~ tourist_access + (1 | Site) + (1| Transect),
+priors <- prior(normal(0, 1), class = "Intercept") +
+  prior(normal(0, 3), class = "b") +
+  prior(gamma(0.01, 0.01), class = "phi")
+
+form <- bf(count_groupcode | trials(total) ~ tourist_access + (1 | Site),
            family = beta_binomial(link = "logit"))
-model2 <- brm(form,
-             data=all_data)
+
+model2 <- brm(form, 
+              data = all_data,
+              prior = priors,
+              chains = 3,
+              iter = 2000,
+              warmup = 1000,
+              thin = 10,
+              sample_prior = "only",
+              backend = "rstan",
+              control = list(adapt_delta = 0.99, max_treedepth = 20))
+
 model2 |> conditional_effects() |> plot()
+
+model2 <- model2 |> 
+  update(sample_prior = "yes")
+
 model2 |> plot()
 
 get_prior(form, data=all_data)
 
 summary(model2)
-=======
+
 summary(model1)
 
-model1$fit |> stan_trace()
+model2$fit |> stan_trace()
 
-model1$fit |> stan_ac()
+model2$fit |> stan_ac()
 
-model1$fit |> stan_rhat()
+model2$fit |> stan_rhat()
 
-model1$fit |> stan_ess()
+model2$fit |> stan_ess()
 
-model1 |> pp_check(type = "dens_overlay", ndraws = 100)
+model2 |> pp_check(type = "dens_overlay", ndraws = 100)
 
-resids <- model1 |> make_brms_dharma_res(integerResponse = FALSE)
+resids <- model2 |> make_brms_dharma_res(integerResponse = FALSE)
 
 testUniformity(resids)
 
@@ -239,15 +244,29 @@ plotResiduals(resids, form = factor(rep(1, nrow(all_data))))
 plotResiduals(resids)
 
 testDispersion(resids)
-<<<<<<< HEAD
-``
->>>>>>> 8ea8d0f00a93e74bba6572529d8e837b80992256
-=======
 
 summary(model1)
 
-model1 |> emmeans(~ tourist_access, type = "response")
+model2 |> emmeans(~ tourist_access, type = "response")
 
-model1 |> emmeans(~ tourist_access, type = "response") |>
+model2 |> emmeans(~ tourist_access, type = "response") |>
   pairs()
->>>>>>> 5ab1b7f0cd791e66a48ba4abe3060336a0f22963
+
+model2 |> 
+  emmeans(~tourist_access) |> 
+  regrid() |> 
+  pairs() |> 
+  gather_emmeans_draws() |> 
+  ggplot(aes(x = .value, y = contrast)) +
+  stat_halfeye(aes(fill = after_stat(level)), .width = c(0.66, 0.95, 1)) +
+  scale_fill_brewer() +
+  geom_vline(xintercept = 0,  linetype = "dashed")
+
+model2 |> 
+  bayes_R2(summary = FALSE) |> 
+  median_hdci()
+
+plot3 <- all_data |> ggplot() +
+  geom_boxplot(aes(x = Site, y = cover, fill = tourist_access)) +
+  ggtitle("Hard Coral Cover") +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
