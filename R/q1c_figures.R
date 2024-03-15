b196dec5347bdfc4d6d2af6178256c174ebd28c5
Genesis <gzustares@gmail.com>
2024-03-15 13:18:20 +0800

    new figures for q1c and updated report

--- R/q1c_figures.R
+++ R/q1c_figures.R
@@ -325,14 +325,37 @@ sur_map.e.n<-sur_map.e.n +
 f4<-(sur_map.w+sur_map.e.s)/sur_map.e.n
 ## ----- end 
 
-
-sur_map.e.n
 ggsave(sur_map.w, filename="../outputs/figures/q1c_MapHCC_West.png")
 ggsave(sur_map.e.n, filename="../outputs/figures/q1c_MapHCC_East_North.png")
 ggsave(sur_map.e.s, filename="../outputs/figures/q1c_MapHCC_East_South.png")
 
 
 ## ----- figure5
+# Predictions
+newdf<- df |> 
+  mutate(year=year(date),f.Side=as.factor(Side), 
+         f.site_name=as.factor(site_name), f.year=as.factor(year) ) |> 
+  select(f.Side,f.year,f.site_name) |> 
+  distinct() |> 
+  mutate(total=1, Var=paste0("V", seq(1:16)),
+         inc=case_when(
+           (f.site_name %in% a.sites) & (f.year=="2021") ~ FALSE,
+           .default=TRUE
+         ))
+
+map.draws <- posterior_epred(model1, newdata = newdf)
+
+pred.df<-map.draws |> 
+  as.data.frame() |>  
+  pivot_longer(cols=everything(),names_to = "Var") |> 
+  left_join(newdf) |> 
+  filter(inc==TRUE) |> 
+  group_by(f.Side,f.site_name) |> 
+  summarise(median_hdci(value) ) |> 
+  rename(Side=f.Side,site_name=f.site_name, HCC.median=y, HCC.min=ymin,HCC.max=ymax) |> 
+  mutate(HCC.median=HCC.median*100, HCC.min=HCC.min*100, HCC.max=HCC.max*100) |> 
+  left_join(sites)
+
 f5<-pred.df |> 
   ggplot(aes(x=site_name, y=HCC.median))+
   geom_point(position = position_dodge(width=0.9), size=3) +
@@ -342,4 +365,197 @@ f5<-pred.df |>
   theme_bw() +
   theme(panel.grid=element_blank(),
         axis.text.x=element_text(angle = 45, hjust = 1))
+## ---- end
+
+## ---- figure6
+## Figure 6: Map of HCC categories across Palawan ####
+
+# Predictions
+newdf<- df |> 
+  mutate(year=year(date),f.Side=as.factor(Side), 
+         f.site_name=as.factor(site_name), f.year=as.factor(year) ) |> 
+  select(f.Side,f.year,f.site_name) |> 
+  distinct() |> 
+  mutate(total=1, Var=paste0("V", seq(1:16)),
+         inc=case_when(
+           (f.site_name %in% a.sites) & (f.year=="2021") ~ FALSE,
+           .default=TRUE
+         ))
+
+map.draws <- posterior_epred(model1, newdata = newdf)
+
+pred.df<-map.draws |>
+  as.data.frame() |>  
+  pivot_longer(cols=everything(),names_to = "Var") |> 
+  left_join(newdf) |> 
+  filter(inc==TRUE) |> 
+  select(-Var,-total,-inc) |> 
+  group_by(f.Side,f.site_name, f.year) |> 
+  summarise_draws(median, HDInterval::hdi,
+                  D=~mean(.x>0 & .x<0.22),
+                  C=~mean(.x>=0.22 & .x<0.33),
+                  B=~mean(.x>=0.33 & .x< 0.44),
+                  A=~mean(.x>= 0.44)) |> 
+    mutate(catHCC=c("A", "B","C","D")[which.max(c(A, B, C, D))],
+           catHCC=as.factor(catHCC)) |> 
+  rename(site_name=f.site_name) |> 
+  left_join(sites)
+  
+  
+ 
+
+# Map 1: HCC West
+# sur_map.w<- sur_map.w +
+#   geom_point(data = pred.df |> 
+#                filter(Side=="West"), 
+#              aes(y = site_latitude, x = site_longitude, size=HCC.median), 
+#              shape = 16, color="blue", alpha=0.35) +
+#   scale_size_continuous(range=c(1,8)) +
+#   geom_text(data = pred.df |> 
+#               filter(Side=="West"), 
+#             aes(y = site_latitude, x = site_longitude, label=site_name), size=2)+
+#   ggtitle("A. Palawan West")+
+#   theme(axis.text = element_text(size=5),
+#         axis.title=element_blank(),
+#         legend.position = "none")
+
+
+# Zoom in on a specific area
+# HCC El Nido
+
+sur_map_El <- pal_map +
+  ggspatial::annotation_scale(location = "br",
+                              width_hint =  0.4,
+                              bar_cols = c("grey20", "white"))+
+  annotation_north_arrow(location = "br",
+                         which_north = "true", 
+                         pad_x = unit(0.2, "npc"),
+                         pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  coord_sf(xlim = c(119.2, 119.51),
+           ylim = c(11.03,11.29), expand = FALSE)
+
+map_el <- sur_map_El +
+  geom_point(data = pred.df |> 
+             filter(site_name=="PAMO_Helicopter"), 
+           aes(y = site_latitude, x =site_longitude, color=catHCC), 
+           shape = 16, size=5) +
+  scale_color_manual(values = c("green", "blue", "yellow", "red"),
+                     limits = c("A","B","C","D"),
+                     breaks = c("A","B","C","D"),name="Category") +
+  geom_text(data = pred.df |> 
+              filter(site_name=="PAMO_Helicopter"), 
+            aes(y = site_latitude+-0.02, x = site_longitude-0.02, label=site_name), size=2)+
+  ggtitle("A. El Nido")+
+  theme(axis.text = element_text(size=5),
+        axis.title=element_blank())
+
+
+# Zoom in to Port Barton
+
+sur_map_pb <- pal_map +
+  ggspatial::annotation_scale(location = "br",
+                              width_hint =  0.4,
+                              bar_cols = c("grey20", "white"))+
+  annotation_north_arrow(location = "br",
+                         which_north = "true", 
+                         pad_x = unit(0.2, "npc"),
+                         pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  coord_sf(xlim = c(119.06, 119.28),
+           ylim = c(10.35,10.6), expand = FALSE)
+
+
+SVmap <- sur_map_pb +
+  geom_point(data = pred.df |> 
+               filter(site_name %in% c("WPU_PB_Aquarium", "WPU_PB_Fantastic", "WPU_PB_Manta")), 
+             aes(y = site_latitude, x =site_longitude, color=catHCC), 
+             shape = 16, size=5) +
+  scale_color_manual(values = c("green", "blue", "yellow", "red"),
+                     limits = c("A","B","C","D"),
+                     breaks = c("A","B","C","D"),name="Category") +
+  geom_text(data = pred.df |> 
+              filter(site_name %in% c("WPU_PB_Aquarium", "WPU_PB_Fantastic", "WPU_PB_Manta")), 
+            aes(y = site_latitude+-0.02, x = site_longitude-0.02, label=site_name), size=2)+
+  ggtitle("B. San Vicente")+
+  theme(axis.text = element_text(size=5),
+        axis.title=element_blank())
+
+# Zoom in to PPC Bay
+
+sur_map_ppb <- pal_map +
+  ggspatial::annotation_scale(location = "br",
+                              width_hint =  0.4,
+                              bar_cols = c("grey20", "white"))+
+  annotation_north_arrow(location = "br",
+                         which_north = "true", 
+                         pad_x = unit(0.2, "npc"),
+                         pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  coord_sf(xlim = c(118.6, 118.9),
+           ylim = c(9.65,10), expand = FALSE)
+
+PPCmap <- sur_map_ppb +
+  geom_point(data = pred.df |> 
+               filter(site_name %in% c("DENR _5", "DENR_3", "DENR_4", "DENR_6", "DENR_Snake_S3", "White Beach")), 
+             aes(y = site_latitude, x =site_longitude, color=catHCC), 
+             shape = 16, size=5) +
+  scale_color_manual(values = c("green", "blue", "yellow", "red"),
+                     limits = c("A","B","C","D"),
+                     breaks = c("A","B","C","D"),name="Category") +
+  geom_text(data = pred.df |> 
+              filter(site_name %in% c("DENR _5", "DENR_3", "DENR_4", "DENR_6", "DENR_Snake_S3", "White Beach")), 
+            aes(y = site_latitude+-0.01, x = site_longitude-0.01, label=site_name), size=2)+
+  ggtitle("C. Puerto Princesa City")+
+  theme(axis.text = element_text(size=5),
+        axis.title=element_blank())
+
+
+# Zoom in to Araceli
+
+sur_map_ar <- pal_map +
+  ggspatial::annotation_scale(location = "br",
+                              width_hint =  0.4,
+                              bar_cols = c("grey20", "white"))+
+  annotation_north_arrow(location = "br",
+                         which_north = "true", 
+                         pad_x = unit(0.2, "npc"),
+                         pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  coord_sf(xlim = c(119.9, 120.15),
+           ylim = c(10.4,10.65), expand = FALSE)
+
+
+Armap <- sur_map_ar +
+  geom_point(data = pred.df |> 
+               filter(str_detect(site_name, "^WWF.*")), 
+             aes(y = site_latitude, x =site_longitude, color=catHCC), 
+             shape = 16, size=5) +
+  scale_color_manual(values = c("green", "blue", "yellow", "red"),
+                     limits = c("A","B","C","D"),
+                     breaks = c("A","B","C","D"),name="Category") +
+  geom_text(data = pred.df |> 
+              filter(str_detect(site_name, "^WWF.*")), 
+            aes(y = site_latitude+-0.01, x = site_longitude-0.01, label=site_name), size=2)+
+  ggtitle("D. Araceli")+
+  theme(axis.text = element_text(size=5),
+        axis.title=element_blank())
+
+f6 <- ( (map_el + SVmap)/ (PPCmap + Armap) & theme(legend.position = "bottom") )+
+  plot_layout(guides = "collect")
+## ---- end 
+
+ggsave(f6, filename="../outputs/figures/q1c_HCCcategory.png")
+
+## ----table.Cat
+
+Pcat.df=pred.df |> 
+  ungroup() |> 
+  rowwise() |> 
+  mutate(Pcat=max(c(D, C, B, A))) |> 
+  select(f.Side, site_name,catHCC,Pcat) |> 
+  rename(Side=f.Side, Site=site_name, Category=catHCC, Probability=Pcat)
+ 
+# kable(Pcat.df)
+
 ## ---- end
